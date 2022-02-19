030bd87ce4191ce906bb4626a7b2a8ff6fa3ff9d
ginpei <ginpei@gmail.com>
2022-02-19 23:17:23 +0000

    fix Unused CSS selector "body"

--- src/lib/layouts/basic/BasicLayout.svelte
+++ src/lib/layouts/basic/BasicLayout.svelte
@@ -3,6 +3,7 @@
 	import Header from '$lib/layouts/basic/BasicHeader.svelte';
 import VStack from '$lib/utils/VStack.svelte';
   import BasicMainContents from './BasicMainContents.svelte';
+  import "./global.css";
 </script>
 
 <VStack className="BasicLayout">
@@ -14,38 +15,3 @@ import VStack from '$lib/utils/VStack.svelte';
 
   <BasicFooter />
 </VStack>
-
-<style global>
-  @import '@fontsource/fira-mono';
-
-  :root {
-    font-family: Arial, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-      Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    --font-mono: 'Fira Mono', monospace;
-    --pure-white: #ffffff;
-    --primary-color: #b9c6d2;
-    --secondary-color: #d0dde9;
-    --tertiary-color: #edf0f8;
-    --accent-color: #ff3e00;
-    --heading-color: rgba(0, 0, 0, 0.7);
-    --text-color: #444444;
-    --background-without-opacity: rgba(255, 255, 255, 0.7);
-    --column-width: 42rem;
-    --column-margin-top: 4rem;
-  }
-
-  body {
-    min-height: 100vh;
-  }
-
-  * {
-    margin: 0;
-  }
-
-  /* TODO extract */
-  fieldset {
-    border-style: none;
-    margin: 0;
-    padding: 0;
-  }
-</style>
