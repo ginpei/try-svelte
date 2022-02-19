3517966ffb6c4d4dc54e6861d42824c581835f5b
ginpei <ginpei@gmail.com>
2022-02-19 20:54:45 +0000

    error box

---
+++ src/lib/complex/ErrorBox.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { toError } from "$lib/utils/errors";
+
+  export let error: unknown;
+
+  const message = toError(error).message;
+</script>
+
+<div class="ErrorBox">
+  <span class="heading">Error:</span>
+  {message}
+</div>
+
+<style>
+  .ErrorBox {
+    border: solid 2px tomato;
+    color: darkred;
+    padding: 1rem;
+  }
+</style>
