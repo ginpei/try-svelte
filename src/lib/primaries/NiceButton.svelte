044170fee9509e25beadd92f7087563fab5ae402
ginpei <ginpei@gmail.com>
2022-02-20 07:08:09 +0000

    click event

--- src/lib/primaries/NiceButton.svelte
+++ src/lib/primaries/NiceButton.svelte
@@ -1,4 +1,14 @@
-<button class="NiceButton">
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function onEvent(event: Event) {
+    dispatch(event.type, event);
+  }
+</script>
+
+<button class="NiceButton" on:click={onEvent}>
   <slot />
 </button>
 
